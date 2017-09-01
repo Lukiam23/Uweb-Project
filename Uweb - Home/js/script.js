3d137a9a64520b06c1622bb96294ab4aa0d2eb94
Lukiam23 <matheus.1596@hot.com>
2017-09-01 15:28:59 -0300

    Add files via upload

---
+++ Uweb - Home/js/script.js
@@ -0,0 +1,48 @@
+var dados=
+{	/*** Mudar o nome das imagens***/
+	1:{"nome":"Monalisa","texto":"<p>Mona Lisa (\"Senhora Lisa\") também conhecida como A Gioconda (em italiano: La Gioconda, \"a sorridente\"; em francês, La Joconde) ou ainda Mona Lisa del Giocondo (\"Senhora Lisa [esposa] de Giocondo\") é a mais notável e conhecida obra de Leonardo da Vinci, um dos mais eminentes homens do Renascimento italiano.</p> <p>Sua pintura foi iniciada em 1503 e é nesta obra que o artista melhor concebeu a técnica do sfumato. O quadro representa uma mulher com uma expressão introspectiva e um pouco tímida. O seu sorriso restrito é muito sedutor, mesmo que um pouco conservador. O seu corpo representa o padrão de beleza da mulher na época de Leonardo. Este quadro é provavelmente o retrato mais famoso na história da arte, senão, o quadro mais famoso e valioso de todo o mundo. Poucos outros trabalhos de arte são tão controversos, questionados, valiosos, elogiados, comemorados ou reproduzidos.</p>","original":"img/Monalisa/Monalisa.png","m1":"img/Monalisa/m1.png","m2":"img/Monalisa/m2.png","m3":"img/Monalisa/m3.png","m4":"img/Monalisa/m4.png","m5":"img/Monalisa/m5.png","m6":"img/Monalisa/m6.png"},
+	2:{"nome":"O Grito","texto":"<p>O Grito (em norueguês Skrik) é uma série de quatro pinturas do norueguês Edvard Munch, a mais célebre das quais datada de 1893. A obra representa uma figura andrógina num momento de profunda angústia e desespero existencial.</p> <p>O plano de fundo é a doca de Oslofjord (em Oslo) ao pôr-do-Sol. O Grito é considerado como uma das obras mais importantes do movimento expressionista e adquiriu um estatuto de ícone cultural, a par da Mona Lisa de Leonardo da Vinci.</p>","original":"img/Grito/Grito.png","m1":"img/Grito/m1.png","m2":"img/Grito/m2.png","m3":"img/Grito/m3.png","m4":"img/Grito/m4.png","m5":"img/Grito/m5.png","m6":"img/Grito/m6.png"},
+	3:{"nome":"Abaporu","texto":"<p> Abaporu é uma pintura a óleo da artista brasileira Tarsila do Amaral. Hoje é a tela brasileira mais valorizada no mundo, tendo alcançado o valor de US$ 2,5 milhões, pago pelo colecionador argentino Eduardo Costantin em 1995 em leilão realizado na Christies. Anteriormente pertencia ao empresário brasileiro Raul Forbes desde 1985.</p> <p> Foi pintada em óleo sobre tela em 1928 por Tarsila do Amaral como presente de aniversário ao escritor Oswald de Andrade, seu marido na época. O nome da obra foi conferido por ele e pelo poeta Raul Bopp, que indagou a Oswald ao ver o quadro: \"Vamos fazer um movimento em torno desse quadro?\" Os dois escritores escolheram um nome para a obra, que veio a ser Abaporu, que vem dos termos em tupi aba (homem), pora (gente) e ú (comer), significando \"homem que come gente\" . E também é uma referência para a criação da Antropofagia modernista brasileira, ou Movimento Antropofágico, que se propunha a deglutir a cultura estrangeira e adaptá-la ao Brasil. A obra encontra-se exposta no Museu de arte latino-americana de Buenos Aires (MALBA).</p>","original":"img/Abaporu/Abaporu.png","m1":"img/Abaporu/m1.png","m2":"img/Abaporu/m2.png","m3":"img/Abaporu/m3.png","m4":"img/Abaporu/m4.png","m5":"img/Abaporu/m5.png","m6":"img/Abaporu/m6.png"},
+	4:{"nome":"A persistência da memória","texto":"<p> A Persistência da Memória (em espanhol: La persistencia de la memoria; em catalão: La persistència de la memòria) é uma pintura de 1931 de Salvador Dalí. A pintura está localizada na coleção do Museu de Arte Moderna (MoMA) de Nova Iorque desde 1934. É amplamente reconhecida e frequentemente referenciada na cultura popular.</p><p> Em sua autobiografia, Dalí conta que levou duas horas para pintar a maior parte da obra (do total de menos de cinco horas), enquanto esperava sua esposa, Gala, voltar do teatro. Neste dia, o pintor se sentira cansado e com uma leve dor de cabeça, não indo ao teatro com sua esposa e amigos. Ao retornar do filme, Dalí mostrou a obra a sua esposa, vendo em sua face a \"contração inequívoca de espanto e admiração\". Ele então, a perguntou se ela achava que em três anos ela esqueceria aquela imagem, tendo como resposta que \"ninguém poderia esquecê-lá uma vez vista\". Mas muita gente quer saber o que essa imagem representa. \“Toda a minha ambição no campo pictórico é materializar as imagens da irracionalidade concreta com a mais imperialista fúria da precisão\”. Esta frase de Dalí resume a pintura em questão; os elementos irreais – relógios derretidos – misturam-se com imagens familiares aos olhos humanos, criando uma impressão de que eles realmente estão ali.</p>","original":"img/Memoria/Memoria.png","m1":"img/Memoria/m1.png","m2":"img/Memoria/m2.png","m3":"img/Memoria/m3.png","m4":"img/Memoria/m4.png","m5":"img/Memoria/m5.png","m6":"img/Memoria/m6.png"},
+	5:{"nome":"A última ceia","texto":"<p> A Última Ceia (em italiano: L'Ultima Cena e também I1 Cenacolo) é um afresco de Leonardo da Vinci para a igreja de seu protetor, o Duque Lodovico Sforza. Representa a cena da última ceia de Jesus com os Apóstolos, antes de ser preso e crucificado como descreve a Bíblia. É um dos maiores bens conhecidos e estimados do mundo.</p><p> O trabalho se encontra no convento de Santa Maria Delle Grazie em Milão,Itália que o Duque mandou construir para, entre outras coisas, servir de lugar para sepultar seus familiares. O tema era uma tradição para refeitórios, mas a interpretação de Leonardo deu um maior realismo e profundidade ao lugar. Leonardo da Vinci passou grande parte destes três anos dando atenção integral a esta pintura o que era fato raro para um pintor versátil e do seu quilate. Sofreu agressões ao longo do tempo desde a abertura de uma porta pelos padres até ao bombardeio aéreo na Segunda Guerra Mundial.</p>","original":"img/Ceia/Ceia.png","m1":"img/Ceia/m1.png","m2":"img/Ceia/m2.png","m3":"img/Ceia/m3.png","m4":"img/Ceia/m4.png","m5":"img/Ceia/m5.png","m6":"img/Ceia/m6.png"},
+	6:{"nome":"Homem Vitruviano","texto":"<p>O Homem Vitruviano de Leonardo da Vinci é um desenho famoso que acompanhava as notas feitas pelo artista por volta do ano 1490 num dos seus diários. Descreve uma figura masculina nua separada e simultaneamente em duas posições sobrepostas com os braços inscritos num círculo e num quadrado. A cabeça é calculada como sendo um oitavo da altura total. Às vezes, o desenho e o texto são chamados de Cânone das Proporções.</p><p>O desenho atualmente faz parte da coleção da Gallerie dell'Accademia (Galeria da Academia) em Veneza, Itália. Examinando o desenho, pode ser notado que a combinação das posições dos braços e pernas formam quatro posturas diferentes. As posições com os braços em cruz e os pés são inscritas juntas no quadrado. Por outro lado, a posição superior dos braços e das pernas é inscrita no círculo. Isto ilustra o princípio que na mudança entre as duas posições, o centro aparente da figura parece se mover, mas de fato o umbigo da figura, que é o verdadeiro centro de gravidade, permanece imóvel.</p>","original":"img/Homem/Homem.png","m1":"img/Homem/m1.png","m2":"img/Homem/m2.png","m3":"img/Homem/m3.png","m4":"img/Homem/m4.png","m5":"img/Homem/m5.png","m6":"img/Homem/m6.png"},
+};
+var count=1
+function prox()
+{
+	count++;
+	if(count>6)
+	{
+		count=1
+	};
+	document.getElementById("nome").innerHTML= dados[count]["nome"];
+	document.getElementById("texto").innerHTML= dados[count]["texto"];
+	document.getElementById("original").src=dados[count]["original"];
+	document.getElementById("m1").src=dados[count]["m1"];
+	document.getElementById("m2").src=dados[count]["m2"];
+	document.getElementById("m3").src=dados[count]["m3"];
+	document.getElementById("m4").src=dados[count]["m4"];
+	document.getElementById("m5").src=dados[count]["m5"];
+	document.getElementById("m6").src=dados[count]["m6"];
+
+
+};
+
+function ant()
+{
+	count--;
+	if(count<1)
+	{
+		count=6
+	};
+	document.getElementById("nome").innerHTML= dados[count]["nome"];
+	document.getElementById("texto").innerHTML= dados[count]["texto"];
+	document.getElementById("original").src=dados[count]["original"];
+	document.getElementById("m1").src=dados[count]["m1"];
+	document.getElementById("m2").src=dados[count]["m2"];
+	document.getElementById("m3").src=dados[count]["m3"];
+	document.getElementById("m4").src=dados[count]["m4"];
+	document.getElementById("m5").src=dados[count]["m5"];
+	document.getElementById("m6").src=dados[count]["m6"];
+
+};
